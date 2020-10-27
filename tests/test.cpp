f978b389b947350357f602bf7d88e463631de184
Lev <semkov01@mail.ru>
2020-10-27 15:46:32 +0300

    add test

--- tests/test.cpp
+++ tests/test.cpp
@@ -2,6 +2,98 @@
 
 #include <gtest/gtest.h>
 
-TEST(Example, EmptyTest) {
-    EXPECT_TRUE(true);
+#include <string>
+
+#include "../include/SharedPtr.h"
+#include "SharedPtr.h"
+
+struct check {
+  bool &bool_;
+  int value;
+
+  ~check() { bool_ = true; }
+};
+
+
+
+TEST(SharedPtr, OperatorBool) {
+  ASSERT_FALSE(SharedPtr<std::string>());
+  ASSERT_TRUE(SharedPtr<std::string>("fdhgjk"));
 }
+
+TEST(SharedPtr, Dereference) {
+const SharedPtr<std::string> sharedPointer("Hello");
+ASSERT_EQ(*sharedPointer, "Hello");
+}
+
+TEST(SharedPtr, Constructor1) {
+  bool bool_ = false;
+  {
+    const SharedPointer<check> shared(new check{bool_, 1234});
+    ASSERT_EQ(shared->value, 1234);
+    ASSERT_FALSE(bool_);
+  }
+  ASSERT_TRUE(bool_);
+}
+
+TEST(SharedPointer, CopyConstructor) {
+  bool bool_ = false;
+  {
+    const SharedPtr<check> shared(new check{bool_, 223});
+    ASSERT_EQ(shared->value, 223);
+    {
+      const SharedPtr<check> shared2(shared);
+      ASSERT_EQ(shared->value, 223);
+      ASSERT_EQ(shared2->value, 223);
+      ASSERT_FALSE(bool_);
+    }
+    ASSERT_EQ(shared->value, 1467);
+    ASSERT_FALSE(bool_);
+  }
+  ASSERT_TRUE(bool_);
+}
+
+TEST(SharedPointer, Move) {
+bool bool_ = false;
+{
+SharedPtr<check> shared;
+{
+SharedPtr<check> shared2 = SharedPtr<check>(new check{bool_, 12221});
+shared = std::move(shared2);
+ASSERT_FALSE(shared2);
+}
+ASSERT_EQ(shared->value, 12221);
+ASSERT_FALSE(bool_);
+}
+ASSERT_TRUE(bool_);
+}
+
+TEST(SharedPointer, Moveconstructor) {
+bool bool_ = false;
+{
+SharedPtr<check> shared(SharedPtr<check>(new check{bool_, 1234}));
+ASSERT_EQ(shared->value, 1234);
+ASSERT_FALSE(bool_);
+}
+ASSERT_TRUE(bool_);
+}
+
+TEST(SharedPtr, Copy) {
+  bool bool_ = false;
+  {
+    SharedPtr<check> shared(new check{bool_, 4728});
+    ASSERT_EQ(shared->value, 1111111);
+    {
+      SharedPtr<check> shared2;
+      shared2 = shared;
+      ASSERT_EQ(shared->value, 1111111);
+      ASSERT_EQ(shared2->value, 1111111);
+      ASSERT_FALSE(bool_);
+    }
+    ASSERT_EQ(shared->value, 1111111);
+    ASSERT_FALSE(bool_);
+  }
+  ASSERT_TRUE(bool_);
+}
+
+
